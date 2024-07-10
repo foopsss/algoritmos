249e9b1648c91b13860847f5ea936d268cf2dfc7
Lucas <libenchat@hotmail.com>
2024-07-10 00:31:58 -0300

    Adición de un ejercicio de Pascal
    Simulacro del laboratorio.

---
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/Extra/simulacro.pas
@@ -0,0 +1,66 @@
+{* Crear un programa que lea una lista de calificaciones (números enteros) desde
+un archivo de texto y calcule el promedio de dichas calificaciones, determinando
+cuántos estudiantes obtuvieron una calificación mayor o igual al promedio. El
+programa también debe mostrar el promedio de calificaciones y el número de
+estudiantes que superaron o igualaron el promedio. *}
+
+// Materiales para este ejercicio:
+// https://www.freepascal.org/docs-html/rtl/system/halt.html
+
+program simulacro;
+
+var
+	entrada: TextFile;
+	v_ent: integer;
+
+	suma: LongInt;
+	promedio: real;
+	est_mayor_prom: integer;
+
+procedure inicializar();
+begin
+	v_ent := 0;
+	suma := 0;
+	est_mayor_prom := 0;
+end;
+
+begin
+	assign(entrada, 'Materiales/entrada-simulacro.txt');
+
+	{$I-}
+	reset(entrada);
+	{$I+}
+
+	if IOResult <> 0 then
+		begin
+			writeln('¡Error!');
+			halt(1);
+		end
+	else
+		inicializar();
+		read(entrada, v_ent);
+
+		while not eof(entrada) do
+		begin
+			suma := suma + v_ent;
+			read(entrada, v_ent);
+		end;
+
+		promedio := suma / 1500;
+		reset(entrada);
+
+		while not eof(entrada) do
+		begin
+			if v_ent >= promedio then
+			begin
+				est_mayor_prom := est_mayor_prom + 1;
+			end;
+
+			read(entrada, v_ent);
+		end;
+
+		writeln('Promedio de calificaciones: ', promedio:10:2);
+		writeln('Número de estudiantes con calificación mayor o igual al promedio: ', est_mayor_prom);
+
+		close(entrada);
+end.
