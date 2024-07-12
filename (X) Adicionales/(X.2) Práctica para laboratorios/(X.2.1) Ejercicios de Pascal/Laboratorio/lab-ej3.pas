af4041aa349b23d070eaad4c6e10df2b9ddcf1ba
Lucas <libenchat@hotmail.com>
2024-07-12 12:47:48 -0300

    Adición de un ejercicio de laboratorio de Pascal

---
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/Laboratorio/lab-ej3.pas
@@ -0,0 +1,118 @@
+{* El Registro Civil posee datos de personas en el archivo "entrada-lab-ej3.txt"
+que contiene sus nombres (pueden ser varias palabras, indeterminado, finaliza
+con "&") y fechas de nacimiento (ocho caracteres, ddmmaaaaa).
+
+Se pide: ¿qué porcentaje de personas nacieron en la primera quincena (de cualquier
+mes) en el primer trimestre (de cualquier año), cuyos nombres comienzan además
+con letras consonantes? *}
+
+program EJ3;
+
+uses
+	functions in '../functions.pas';
+
+type
+	sec = TextFile;
+
+var
+	entrada: sec;
+	v_ent: char;
+
+	i, dia_nac, mes_nac: integer;
+	cont_personas, quin_trim_cons: integer;
+	es_consonante, es_quincena, es_trimestre: boolean;
+	porc_quin_trim_cons: real;
+
+procedure inicializar();
+begin
+	cont_personas := 0;
+	quin_trim_cons := 0;
+	dia_nac := 0;
+	mes_nac := 0;
+end;
+
+begin
+	assign(entrada, 'Materiales/entrada-lab-ej3.txt');
+
+	{$I-}
+	reset(entrada);
+	{$I+}
+
+	if IOResult <> 0 then
+		begin
+			writeln('¡Error!');
+			halt(1);
+		end
+	else
+		begin
+			inicializar();
+			read(entrada, v_ent);
+
+			while not eof(entrada) do
+			begin
+				cont_personas := cont_personas + 1;
+
+				if v_ent in ['A', 'E', 'I', 'O', 'U'] then
+					begin
+						es_consonante := false;
+					end
+				else
+					begin
+						es_consonante := true;
+					end;
+
+				while v_ent <> '&' do
+				begin
+					read(entrada, v_ent);
+				end;
+
+				read(entrada, v_ent);
+
+				// dia de nacimiento.
+				dia_nac := dia_nac + CharToInt(v_ent) * 10;
+				read(entrada, v_ent);
+				dia_nac := dia_nac + CharToInt(v_ent);
+
+				if dia_nac <= 15 then
+					begin
+						es_quincena := true;
+					end
+				else
+					begin
+						es_quincena := false;
+					end;
+
+				// Mes de nacimiento.
+				mes_nac := mes_nac + CharToInt(v_ent) * 10;
+				read(entrada, v_ent);
+				mes_nac := mes_nac + CharToInt(v_ent);
+
+				if mes_nac <= 3 then
+					begin
+						es_trimestre := true;
+					end
+				else
+					begin
+						es_trimestre := false;
+					end;
+
+				if (es_consonante) and (es_quincena) and (es_trimestre) then
+				begin
+					quin_trim_cons := quin_trim_cons + 1;
+				end;
+
+				for i := 1 to 6 do
+				begin
+					read(entrada, v_ent);
+				end;
+
+				dia_nac := 0;
+				mes_nac := 0;
+			end;
+
+			porc_quin_trim_cons := (quin_trim_cons * 100) / cont_personas;
+			writeln('Porcentaje de personas: ', porc_quin_trim_cons:4:2);
+
+			close(entrada)
+		end;
+end.
