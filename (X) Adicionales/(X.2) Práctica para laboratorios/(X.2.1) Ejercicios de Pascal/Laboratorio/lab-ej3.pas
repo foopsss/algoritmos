b089e94fdd512903f17af53d26889313671a65d0
Lucas <libenchat@hotmail.com>
2024-10-05 16:00:49 -0300

    Correcciones de indentanción en cosas de Pascal
    Indentación corregida en:
    
    * pas.sh y functions.pas
    * Laboratorio
    * Extra

--- (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/Laboratorio/lab-ej3.pas
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/Laboratorio/lab-ej3.pas
@@ -9,107 +9,94 @@ con letras consonantes? *}
 program EJ3;
 
 uses
-	functions in '../functions.pas';
+    functions in '../functions.pas';
 
 type
-	sec = TextFile;
+    sec = TextFile;
 
 var
-	entrada: sec;
-	v_ent: char;
+    entrada: sec;
+    v_ent: char;
 
-	i, dia_nac, mes_nac: integer;
-	cont_personas, quin_trim_cons: integer;
-	es_consonante, es_quincena, es_trimestre: boolean;
-	porc_quin_trim_cons: real;
+    i, dia_nac, mes_nac: integer;
+    cont_personas, quin_trim_cons: integer;
+    es_consonante, es_quincena, es_trimestre: boolean;
+    porc_quin_trim_cons: real;
 
 procedure inicializar();
 begin
-	cont_personas := 0;
-	quin_trim_cons := 0;
-	dia_nac := 0;
-	mes_nac := 0;
+    cont_personas := 0;
+    quin_trim_cons := 0;
+    dia_nac := 0;
+    mes_nac := 0;
 end;
 
 begin
-	assign(entrada, 'Materiales/entrada-lab-ej3.txt');
+    assign(entrada, 'Materiales/entrada-lab-ej3.txt');
 
-	{$I-}
-	reset(entrada);
-	{$I+}
+    {$I-}
+    reset(entrada);
+    {$I+}
 
-	if IOResult <> 0 then
-		error_lectura_archivo()
-	else
-		begin
-			inicializar();
-			read(entrada, v_ent);
+    if IOResult <> 0 then
+        error_lectura_archivo()
+    else
+        begin
+            inicializar();
+            read(entrada, v_ent);
 
-			while not eof(entrada) do
-			begin
-				cont_personas := cont_personas + 1;
+            while not eof(entrada) do
+            begin
+                cont_personas := cont_personas + 1;
 
-				if v_ent in ['A', 'E', 'I', 'O', 'U'] then
-					begin
-						es_consonante := false;
-					end
-				else
-					begin
-						es_consonante := true;
-					end;
+                if v_ent in ['A', 'E', 'I', 'O', 'U'] then
+                    es_consonante := false
+                else
+                    es_consonante := true;
 
-				while v_ent <> '&' do
-				begin
-					read(entrada, v_ent);
-				end;
+                while v_ent <> '&' do
+                begin
+                    read(entrada, v_ent);
+                end;
 
-				read(entrada, v_ent);
+                read(entrada, v_ent);
 
-				// dia de nacimiento.
-				dia_nac := dia_nac + CharToInt(v_ent) * 10;
-				read(entrada, v_ent);
-				dia_nac := dia_nac + CharToInt(v_ent);
+                // Día de nacimiento.
+                dia_nac := dia_nac + CharToInt(v_ent) * 10;
+                read(entrada, v_ent);
+                dia_nac := dia_nac + CharToInt(v_ent);
 
-				if dia_nac <= 15 then
-					begin
-						es_quincena := true;
-					end
-				else
-					begin
-						es_quincena := false;
-					end;
+                if dia_nac <= 15 then
+                    es_quincena := true
+                else
+                    es_quincena := false;
 
-				// Mes de nacimiento.
-				mes_nac := mes_nac + CharToInt(v_ent) * 10;
-				read(entrada, v_ent);
-				mes_nac := mes_nac + CharToInt(v_ent);
+                // Mes de nacimiento.
+                mes_nac := mes_nac + CharToInt(v_ent) * 10;
+                read(entrada, v_ent);
+                mes_nac := mes_nac + CharToInt(v_ent);
 
-				if mes_nac <= 3 then
-					begin
-						es_trimestre := true;
-					end
-				else
-					begin
-						es_trimestre := false;
-					end;
+                if mes_nac <= 3 then
+                    es_trimestre := true
+                else
+                    es_trimestre := false;
 
-				if (es_consonante) and (es_quincena) and (es_trimestre) then
-				begin
-					quin_trim_cons := quin_trim_cons + 1;
-				end;
+                if (es_consonante) and (es_quincena) and (es_trimestre) then
+                begin
+                    quin_trim_cons := quin_trim_cons + 1;
+                end;
 
-				for i := 1 to 6 do
-				begin
-					read(entrada, v_ent);
-				end;
+                for i := 1 to 6 do
+                begin
+                    read(entrada, v_ent);
+                end;
 
-				dia_nac := 0;
-				mes_nac := 0;
-			end;
+                dia_nac := 0;
+                mes_nac := 0;
+            end;
 
-			porc_quin_trim_cons := (quin_trim_cons * 100) / cont_personas;
-			writeln('Porcentaje de personas: ', porc_quin_trim_cons:4:2);
-
-			close(entrada)
-		end;
+            porc_quin_trim_cons := (quin_trim_cons * 100) / cont_personas;
+            writeln('Porcentaje de personas: ', porc_quin_trim_cons:4:2);
+            close(entrada)
+        end;
 end.
--- (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/functions.pas
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/functions.pas
@@ -8,50 +8,50 @@
 unit functions;
 
 interface
-	function CharToInt(entrada: char): integer;
-	procedure draw_line(length: integer; symbol: char = ' ');
-	procedure error_lectura_archivo();
+    function CharToInt(entrada: char): integer;
+    procedure draw_line(length: integer; symbol: char = ' ');
+    procedure error_lectura_archivo();
 
 implementation
-	function CharToInt(entrada: char): integer;
-	begin
-		case entrada of
-			'0': CharToInt := 0;
-			'1': CharToInt := 1;
-			'2': CharToInt := 2;
-			'3': CharToInt := 3;
-			'4': CharToInt := 4;
-			'5': CharToInt := 5;
-			'6': CharToInt := 6;
-			'7': CharToInt := 7;
-			'8': CharToInt := 8;
-			'9': CharToInt := 9;
-		end;
-	end;
+    function CharToInt(entrada: char): integer;
+    begin
+        case entrada of
+            '0': CharToInt := 0;
+            '1': CharToInt := 1;
+            '2': CharToInt := 2;
+            '3': CharToInt := 3;
+            '4': CharToInt := 4;
+            '5': CharToInt := 5;
+            '6': CharToInt := 6;
+            '7': CharToInt := 7;
+            '8': CharToInt := 8;
+            '9': CharToInt := 9;
+        end;
+    end;
 
-	procedure draw_line(length: integer; symbol: char = ' ');
-	var
-		i: integer;
-	begin
-		for i := 0 to (length - 1) do
-		begin
-			if symbol = ' ' then
-				write('-')
-			else
-				write(symbol);
-		end;
+    procedure draw_line(length: integer; symbol: char = ' ');
+    var
+        i: integer;
+    begin
+        for i := 0 to (length - 1) do
+        begin
+            if symbol = ' ' then
+                write('-')
+            else
+                write(symbol);
+        end;
 
-		if symbol = ' ' then
-			writeln('-')
-		else
-			writeln(symbol);
-	end;
+        if symbol = ' ' then
+            writeln('-')
+        else
+            writeln(symbol);
+    end;
 
-	procedure error_lectura_archivo();
-	begin
-		writeln('¡Error!');
-		writeln('No se pudo abrir el archivo.');
-		halt(1);
-	end;
+    procedure error_lectura_archivo();
+    begin
+        writeln('¡Error!');
+        writeln('No se pudo abrir el archivo.');
+        halt(1);
+    end;
 
 end.
