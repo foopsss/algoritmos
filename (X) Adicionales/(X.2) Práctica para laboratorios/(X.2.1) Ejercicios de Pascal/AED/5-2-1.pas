4600040e8ec1849e9ec4e452527ef5a88be863e4
Lucas <libenchat@hotmail.com>
2024-11-12 18:45:34 -0300

    Adición del 5.2.1

---
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/AED/5-2-1.pas
@@ -0,0 +1,26 @@
+program EJ521;
+
+var
+    num_us: LongInt;
+
+procedure inverso(num: LongInt);
+var
+    digito: integer;
+begin
+    if num mod 10 = num then
+        writeln(num)
+    else
+        begin
+            digito := num mod 10;
+            writeln(digito);
+            num := num div 10;
+            inverso(num);
+        end;
+end;
+
+begin
+    write('Introduzca un número: ');
+    readln(num_us);
+    writeln('Dígitos invertidos: ');
+    inverso(num_us);
+end.
