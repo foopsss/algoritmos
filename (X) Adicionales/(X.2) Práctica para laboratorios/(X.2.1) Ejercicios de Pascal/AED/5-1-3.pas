8a28cad9bd67dfb0a283bd6120a0dcf371ce9f44
Lucas <libenchat@hotmail.com>
2024-11-14 18:44:53 -0300

    Adición del 5.1.3

---
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/AED/5-1-3.pas
@@ -0,0 +1,27 @@
+program EJ513;
+
+var
+    a, b, res: integer;
+
+function potencia(base, exp: integer): integer;
+begin
+    if exp = 0 then
+        potencia := 1
+    else
+        if exp = 1 then
+            potencia := base
+        else
+            potencia := base * potencia(base, exp - 1);
+            // Si se quiere ver el paso a paso del proceso, se
+            // puede incluir esto.
+            // writeln(potencia);
+end;
+
+begin
+    write('Introduzca una base: ');
+    readln(a);
+    write('Introduzca un exponente: ');
+    readln(b);
+    res := potencia(a, b);
+    writeln('El resultado de la operación es: ', res);
+end.
