9313458f7c3186230c7940294aeea1cd19ce7c67
Lucas <libenchat@hotmail.com>
2025-01-07 23:00:02 -0300

    Soporte para depuración en c.sh

--- (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.2) Ejercicios de C/c.sh
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.2) Ejercicios de C/c.sh
@@ -9,6 +9,15 @@ compile_program() {
     fi
 }
 
+compile_debugging() {
+    if [ "$platform" == "mgw" ]
+    then
+        x86_64-w64-mingw32-gcc -g $program.c -o "$program-dbg".exe
+    else
+        gcc -g $program.c -o "$program-dbg"
+    fi
+}
+
 run_program() {
     compile_program
     coderr=$?
@@ -33,12 +42,13 @@ delete_files() {
     fi
 }
 
-while getopts l:p:c:r:d: flag;
+while getopts l:p:c:g:r:d: flag;
 do
     case "${flag}" in
         l) cd "$OPTARG";;
         p) platform=$OPTARG;;
         c) program=$OPTARG; compile_program;;
+        g) program=$OPTARG; compile_debugging;;
         r) program=$OPTARG; run_program;;
         d) program=$OPTARG; delete_files;;
     esac
