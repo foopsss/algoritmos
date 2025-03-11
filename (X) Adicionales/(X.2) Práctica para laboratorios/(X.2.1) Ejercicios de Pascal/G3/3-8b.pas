9f7c960ee920ba4b3d400e8de5fbcea499195b8b
Lucas Ivan Benchat Parra <libenchat@hotmail.com>
2025-03-11 03:36:13 -0300

    Modificación de 3-8b.pas
    Signed-off-by: Lucas Ivan Benchat Parra <libenchat@hotmail.com>

--- (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/G3/3-8b.pas
+++ (X) Adicionales/(X.2) Práctica para laboratorios/(X.2.1) Ejercicios de Pascal/G3/3-8b.pas
@@ -138,6 +138,12 @@ end;
 
 procedure procesar_registro();
 begin
+    // procesar_registro es donde hago la parte de
+    // incrementar contadores y verificar si un
+    // empleado es de categoría cuatro.
+    // Lo abstraigo en un procedimiento porque también
+    // debo hacerlo por fuera del ciclo principal.
+    // Explicado debajo del "while not eof(entrada)".
     if reg_emp.categoria = 4 then
     begin
         write(salida_cat_cuatro, reg_emp);
@@ -171,11 +177,6 @@ begin
                 if resg_categoria <> reg_emp.categoria then
                     corte_categoria();
 
-            // procesar_registro es donde hago la parte de
-            // incrementar contadores y verificar si un
-            // empleado es de categoría cuatro. Lo abstraigo
-            // en un procedimiento porque debo hacerlo por
-            // fuera de este ciclo. Explicado justo abajo.
             procesar_registro();
             read(entrada, reg_emp);
         end;
@@ -192,12 +193,15 @@ begin
         corte_planta();
         writeln('Total general de empleados: ', tot_gen);
 
-        // Muestro los nombres de los empleados y cierro
-        // los archivos de entrada y salida.
+        // Muestro los nombres de los empleados.
         draw_line(50, '=');
         writeln('Listado de empleados de la empresa: ');
         mostrar_lista(prim);
+
+        // Cierro los archivos de entrada y salida.
         close(entrada);
+        close(salida_cat_cuatro);
+        close(salida_emp_planta);
     except
         on E: EInOutError do
         begin
